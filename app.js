c3f28d7eccf1b6dfae030f5b8a2b48a560c94c83
patriciafig <patricia.figueroa@ucdenver.edu>
2018-11-26 11:36:32 -0700

    update mongo path

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var passport = require('passport');
 var session = require('express-session');
 
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://root:mainroot1@ds157599.mlab.com:57599/heroku_gl4nd1gd');
+mongoose.connect('mongodb://root:mainroot1@ds157599.mlab.com:57599/heroku_gl4nd1gd');
 //mongoose.connect('mongodb://localhost/27017/csm', { useMongoClient: true }); // use to run locally
-mongoose.connect('mongodb://localhost:27017/csm');
+
 
 
 require('./models/models');
